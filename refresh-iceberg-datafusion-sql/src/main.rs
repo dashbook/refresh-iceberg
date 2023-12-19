f38d98b3b17bdb3c16cdc6d916fb483fa6ba2a21
Jan Kaul <jankaul@mailbox.org>
2023-12-19 09:36:28 +0100

    update dependencies

--- refresh-iceberg-datafusion-sql/src/main.rs
+++ refresh-iceberg-datafusion-sql/src/main.rs
@@ -1,11 +1,13 @@
 use std::{fs, sync::Arc};
 
-use dashbook_catalog::DashbookS3CatalogList;
+use dashtool_common::ObjectStoreConfig;
 use datafusion_iceberg::materialized_view::refresh_materialized_view;
+use iceberg_catalog_sql::SqlCatalogList;
 use iceberg_rust::{
     catalog::{identifier::Identifier, tabular::Tabular, CatalogList},
     error::Error,
 };
+use object_store::{aws::AmazonS3Builder, memory::InMemory, ObjectStore};
 use serde::{Deserialize, Serialize};
 
 #[tokio::main]
@@ -13,7 +15,7 @@ async fn main() -> Result<(), Error> {
     let config_json = fs::read_to_string("/tmp/refresh.json")?;
     let config: Config = serde_json::from_str(&config_json)?;
 
-    let mut parts = config.identifier.split(".");
+    let mut parts = config.identifier.split('.');
     let catalog_name = parts
         .next()
         .ok_or(Error::InvalidFormat("Identifier".to_string()))?
@@ -33,18 +35,35 @@ async fn main() -> Result<(), Error> {
         )))?
         .to_owned();
 
-    let catalog_list = Arc::new(DashbookS3CatalogList::new(
-        &config.access_token,
-        &config.id_token,
-    ));
+    let object_store: Arc<dyn ObjectStore> = match &config.object_store {
+        ObjectStoreConfig::Memory => Arc::new(InMemory::new()),
+        ObjectStoreConfig::S3(s3_config) => {
+            let builder = AmazonS3Builder::new()
+                .with_region(&s3_config.aws_region)
+                .with_bucket_name(config.bucket.clone().expect("No bucket specified."))
+                .with_access_key_id(&s3_config.aws_access_key_id);
+
+            let builder = if let Some(aws_secret_access_key) = &s3_config.aws_secret_access_key {
+                builder.with_secret_access_key(aws_secret_access_key)
+            } else {
+                builder
+            };
+            Arc::new(builder.build()?)
+        }
+    };
+
+    let catalog_list = Arc::new(SqlCatalogList::new(&config.url, object_store).await?);
 
     let catalog = catalog_list
         .catalog(&catalog_name)
         .await
-        .ok_or(Error::NotFound(format!("Catalog"), format!("catalog")))?;
+        .ok_or(Error::NotFound(
+            "Catalog".to_string(),
+            "catalog".to_string(),
+        ))?;
 
     let matview = if let Tabular::MaterializedView(matview) = catalog
-        .load_table(&Identifier::try_new(&vec![
+        .load_table(&Identifier::try_new(&[
             namespace_name.to_owned(),
             table_name.to_owned(),
         ])?)
@@ -59,14 +78,16 @@ async fn main() -> Result<(), Error> {
     refresh_materialized_view(&matview, catalog_list, config.branch.as_deref())
         .await
         .expect("Failed to refresh materialized view.");
+
     Ok(())
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Config {
-    pub access_token: String,
-    pub id_token: String,
+    #[serde(flatten)]
+    pub object_store: ObjectStoreConfig,
+    pub url: String,
     pub identifier: String,
     pub bucket: Option<String>,
     pub branch: Option<String>,
